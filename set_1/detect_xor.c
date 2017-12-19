5e775edb9a7fd532577fb80242f00d0b04456483
Albert Yu <ayu@kcura.com>
2017-12-19 14:55:32 -0600

    Tabs to spaces

--- set_1/detect_xor.c
+++ set_1/detect_xor.c
@@ -6,20 +6,20 @@
 void read_and_print()
 {
 
-	// relative file path
-	const char *filename = "data/4.txt";
-	// open file in read-only mode
-	const char* mode = "r";
-	FILE *file = fopen(filename, mode);
-	printf("Executing method.\n");
-	if (file)
-	{
-		printf("Reading file char by char.\n");
-		int c;
-		while ((c = getc(file)) != EOF)
-		{
-			putchar(c);
-		}
-		fclose(file);
-	}
+    // relative file path
+    const char *filename = "data/4.txt";
+    // open file in read-only mode
+    const char* mode = "r";
+    FILE *file = fopen(filename, mode);
+    printf("Executing method.\n");
+    if (file)
+    {
+        printf("Reading file char by char.\n");
+        int c;
+        while ((c = getc(file)) != EOF)
+        {
+            putchar(c);
+        }
+        fclose(file);
+    }
 }
