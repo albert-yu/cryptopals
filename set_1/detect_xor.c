1bfd128f7ffbe53375b070ca55a7f6d63fb14546
a-yu-relativity <ayu@kcura.com>
2018-03-15 14:27:28 -0500

    Implement way to read strings into memory

--- set_1/detect_xor.c
+++ set_1/detect_xor.c
@@ -30,17 +30,17 @@ void read_and_print()
     // }
  
     // printf("The contents of %s file are :\n", file_name);
-    // int line_count = 0;
+    // int curr_position = 0;
     // while ((ch = fgetc(fp)) != EOF )
     // {
     //     printf("%c", ch);
     //     if (ch == '\n')
     //     {
-    //         line_count++;
+    //         curr_position++;
     //     }
     // }
  
-    // printf("\nLine count: %d", line_count); 
+    // printf("\nLine count: %d", curr_position); 
     // fclose(fp);
 
     //--------------------------------------------------------
@@ -65,10 +65,10 @@ void read_and_print()
     char *line = NULL;
 
     // line count for debugging
-    int line_count = 0;
+    int curr_position = 0;
     while ((line_len = getline(&line, &len, fp)) != -1)
     {       
-        printf("Line #: %d\n", line_count);
+        printf("Line #: %d\n", curr_position);
         char null_byte = '\0';
         char *temp_key = &null_byte;  // holds the "best" key for this line
 
@@ -92,10 +92,10 @@ void read_and_print()
         }
 
         next_iter:
-            line_count++;
+            curr_position++;
     }
 
-    printf("\nLine count: %d", line_count); 
+    printf("\nLine count: %d", curr_position); 
 }
 
 
@@ -148,12 +148,48 @@ int getline(char **lineptr, size_t *n, FILE *stream)
 }
 
 
-void unscramble_all()
+void copy_over_strings(char **destination, char **source, unsigned int num_strings)
+{   
+    unsigned int i;
+    for (i = 0; i < num_strings; i++)
+    {
+        char *line = source[i];
+        size_t str_size = strlen(line) + 1;  // + 1 for null terminator
+
+        // allocate memory for string
+        destination[i] = (char*) malloc((str_size) * sizeof(char));
+
+        // copy string
+        strcpy(destination[i], source[i]);
+        // printf("%s\n", destination[i]);
+    }
+}
+
+
+/*
+ * Frees up the memory used by an array of pointers
+ */
+void delete(char **pointer_arr, unsigned int block_size)
+{
+    unsigned int i;
+    for (i = 0; i < block_size; i++)
+    {
+        free(pointer_arr[i]);
+    }
+
+    free(pointer_arr);
+}
+
+
+/*
+ * Reads file into memory. Returns an array of pointers as well as the number of lines.
+ * Assumes the length of the lines as 60
+ */
+char** read_file(char *filename, unsigned long *num_lines)
 {
     // open the file
-    char file_name[25];
     FILE *fp;
-    fp = fopen("S:/Projects/cryptopals/set_1/data/4.txt", "r");
+    fp = fopen(filename, "r");
 
     if (fp == NULL)
     {
@@ -161,11 +197,83 @@ void unscramble_all()
         exit(EXIT_FAILURE);
     }
 
-    const int STRING_LEN = 61;  // 60 + null term
+    // file opened successfully, so dynamically allocate memory for it
+    const int LINE_BUF_SIZE = 64;  // line_length should be no more than 60
+    unsigned long block_size = 256;        // for the array of pointers
+    unsigned long curr_position = 0;          // actual no. of lines in block
+    char **all_lines = (char **) malloc(block_size * sizeof(char*));
 
-    // keep track of most likely candidates
-    long long max_score = 0; // keep track of max score
-    char *candidate = (char*)malloc(STRING_LEN * sizeof(*candidate));
+    all_lines[curr_position] = 
+        (char*) malloc((LINE_BUF_SIZE) * sizeof(char));
+    int line_ptr_offset = 0;  // points to position within the line
+
+    char ch;
+    while ((ch = fgetc(fp)) != EOF )
+    {
+        // printf("%c", ch);
+        if (ch == '\n')
+        {
+            // copy null terminator instead of newline at end of string
+            all_lines[curr_position][line_ptr_offset] = '\0';
+            curr_position++;
+
+            if (curr_position >= block_size - 1)
+            {
+                // printf("Reallocating\n");
+                // double the block size to account for more lines
+                block_size *= 2;
+
+                // create temp block with bigger size
+                char **temp = (char**) malloc(block_size * sizeof(char*));
+
+                // copy over old contents over
+                copy_over_strings(temp, all_lines, curr_position); 
+                char **old = all_lines;
+                all_lines = temp;
+                delete(old, curr_position);      
+            }
+
+            // allocate more memory
+            all_lines[curr_position] = 
+                (char*) malloc((LINE_BUF_SIZE) * sizeof(char));
+
+            line_ptr_offset = 0;
+            continue;
+        }
+        // printf("Line count: %d\n", curr_position);
+        // copy character over to array of pointers
+        all_lines[curr_position][line_ptr_offset] = ch;
+        line_ptr_offset++;
+    }
+ 
+    unsigned long line_count = curr_position + 1;
+    // printf("\nLine count: %d\n", line_count); 
+    
+    *num_lines = line_count;
+    if (fp)
+    {
+        fclose(fp);
+    }
+    
+    return all_lines;
+}
+
+
+
+/*
+ * Unscrambles an array of strings, choosing the one with the highest score
+ */
+void unscramble_all(char **hex_strings, unsigned long num_strings)
+{
+    // expected string length
+    const int STRING_LEN = 60; 
+
+    // keep track of max score
+    long long max_score = 0; 
+
+    // keep track of most likely candidate
+    char *candidate = (char*)malloc((STRING_LEN + 1) * sizeof(*candidate));
+
     char the_key = '\0';  // the XOR cipher never tests 0, so 
                           // we can return this if no suitable
                           // key is found
@@ -174,85 +282,24 @@ void unscramble_all()
     // if text is English
     long long *freq_table = get_frequency_table();
 
-    // read the file
-    size_t len = 0;
-    ssize_t line_len;
-    char *line = NULL;
-
-    // line count for debugging
-    int line_count = 0;
-    while ((line_len = getline(&line, &len, fp)) != -1)
-    {       
-        printf("Line #: %d\n", line_count);
-        char null_byte = '\0';
-        char *temp_key = &null_byte;  // holds the "best" key for this line
-
-        if (line_len != STRING_LEN)
-        {
-            goto next_iter;
-        }
-        // copy the line to a temporary buffer
-        // to avoid side effects
-        char *temp_str = malloc(line_len * sizeof(*temp_str));
-        if (temp_str)
-        {         
-            strcpy(temp_str, line);
-            printf("line: %s\n", temp_str);
-            // unscramble and store key in temp_key
-            char *curr_str = unscramble(temp_str, temp_key);           
-            long long curr_score = eval_frequency(freq_table, curr_str, line_len);
-            
-            // long long curr_score = 1;
-            if (curr_score > max_score)
-            {
-                max_score = curr_score;
-                // copy current string to candidate string
-                // candidate = curr_str;
-                strcpy(candidate, curr_str);
-                the_key = *temp_key;
-            }
-            // else
-            // {
-            //     free(curr_str);
-            // }
-            printf("cleanup\n");
-            if (curr_str)
-                free(curr_str);
-
-            free(temp_str);
-        }        
-
-        else
-        {
-            printf("malloc failed! \n");
-            break;
-        }
-
-        next_iter:
-            line_count++;
-    }
-
-    printf("Line count: %d\n", line_count);
-    fclose(fp);
-    free(line);
-    free(freq_table);
-    // print out results
-    if (the_key != '\0')
-    {
-        printf("The key is: %c\n", the_key);
-        printf("Unscrambled: %s\n", candidate);
-    }
-    else
-    {
-        printf("Error: unable to unscramble");
-    }
 }
 
 
 void prob4_test()
 {
     printf("Running test for problem 4...\n");
-    unscramble_all();
-    // read_and_print();
+    // unscramble_all();
+    char *filename = "S:/Projects/cryptopals/set_1/data/4.txt";
+    unsigned long zero = 0;
+    unsigned long *num_lines = &zero;  // store number of lines here
+    char **lines = read_file(filename, num_lines);
+    printf("Number of lines: %d\n", *num_lines);
+    // print out the lines
+    unsigned long i;
+    for (i = 0; i < *num_lines; i++)
+    {
+        printf("%s\n", lines[i]);
+    }
+    
     printf("\n");
 }
