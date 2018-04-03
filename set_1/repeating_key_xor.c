475df6aa13a9a84f0c4dd3fead2d22eabcfb1ec4
a-yu-relativity <ayu@kcura.com>
2018-04-03 11:26:26 -0500

    Not getting correct answer, but some progress made

--- set_1/repeating_key_xor.c
+++ set_1/repeating_key_xor.c
@@ -7,7 +7,7 @@
 /*
  * Convert an ASCII string to bytes, then to hex
  */
-char* convert_to_hex(char* input)
+char* convert_to_hex(char *input)
 {
 
 }
@@ -15,6 +15,13 @@ char* convert_to_hex(char* input)
 
 void encrypt(char *input, const char *key, char *output)
 {
+    // check if output has been allocated at all
+    if (!output)
+    {
+        perror("Need to allocate memory for output.\n");
+        exit(EXIT_FAILURE);
+    }
+
     // contains current index
     size_t iter = 0;
 
@@ -27,12 +34,19 @@ void encrypt(char *input, const char *key, char *output)
         // take the modulus
         size_t key_offset = iter % key_len;
 
+        // int val = hex_val(c) 
+        char xord = c ^ key[key_offset];
+        char xord_hex = digit_to_hex(xord);
+        // printf("%c", xord_hex);
         // assuming that output has been allocated
-        output[iter] = c;
+        output[iter] = xord_hex;
 
         iter++;
         input++;
     }
+
+    // terminate
+    output[iter] = '\0';
 }
 
 
@@ -42,4 +56,10 @@ void prob5_test()
     char *key = "ICE";
     char *stanza = "Burning 'em, if you ain't quick and nimble \nI go crazy when I hear a cymbal";
     char *expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272 \na282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+
+    // alloc memory for output
+    size_t stanza_len = strlen(stanza) * 2 + 1;
+    char *actual = (char*) malloc(stanza_len * sizeof(*actual));
+    encrypt(stanza, key, actual);
+    printf("Actual: %s\n", actual);
 }
