de9d9476e94bba8f7a5359a058a230eaafcea86c
Albert Yu <albert.yu@relativity.com>
2018-12-07 12:50:23 -0600

    Fix mem leak in problem 2

--- set_1/fixed_xor.c
+++ set_1/fixed_xor.c
@@ -11,9 +11,20 @@ char* fixed_xor(char *input1, char *input2) {
     int length1 = strlen(input1);
     int length2 = strlen(input2);
     if (length1 != length2) {
-        return "";
+        char *empty_str = calloc(1, sizeof(char));
+        if (empty_str) {
+            // return empty string
+            return (char*)calloc(1, sizeof(char));
+        }
+        // out of memory
+        fprintf(stderr, "Out of memory. Exiting...\n");
+        exit(1);
     }
-    char *ret_val = calloc(length1, sizeof(*ret_val));
+    char *ret_val = calloc(length1 + 1, sizeof(*ret_val));
+    if (ret_val == NULL) {
+        fprintf(stderr, "Out of memoery. Exiting...\n");
+        exit(1);
+    }
     for (int i = 0; i < length1; i++) {
         int val = hex_val(input1[i]) ^ hex_val(input2[i]);
         ret_val[i] = digit_to_hex(val);
@@ -36,6 +47,8 @@ void fixed_xor_test() {
         printf("Expected: \t[%s]\n", expected);
         printf("Actual: \t[%s]\n", result);
     }
+    if (result)
+        free(result);
 }
 
 void prob2_test() {
